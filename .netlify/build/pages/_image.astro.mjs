15c1c8b282d4b746ea570a4c6fef3b9b5a11039e
Carla Wilson <carla.wilson@bld-marketing.com>
2025-06-24 15:19:55 -0400

    style/add contact page

--- .netlify/build/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_3tReuNo3.mjs';
-export { renderers } from '../renderers.mjs';
--- nichiha/schemaTypes/index.js
+++ nichiha/schemaTypes/index.js
@@ -24,7 +24,7 @@ export const schemaTypes = [
   event,
   blockContent,
   page,
-  linkComponent,
+  // linkComponent,
   navigation,
 
 ];
